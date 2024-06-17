08c652c70a0e0b8a95b4179c941abf2436de2dc8
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-06-17 08:52:20 -0400

    json, webpack set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+    const element = document.createElement('div');
+    
+    console.log("hello");
+    return element;
+  }
+ 
+  document.body.appendChild(component());
